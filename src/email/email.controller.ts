388c4a7027a1c4476a5aa867f51774eed9234d84
jalvaradoWD <josealvarado1337@gmail.com>
2021-08-12 22:21:31 -0500

    new change

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -3,22 +3,51 @@ import {config} from 'dotenv';
 import sgMail from '@sendgrid/mail';
 import fs from 'fs';
 import path from 'path';
+import QuickChart from 'quickchart-js';
 config();
 
 const router = Router();
 
 router.get('/', async (req, res) => {
   try {
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: 'My First dataset',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45],
+        },
+      ],
+    };
+    const config = {
+      type: 'line',
+      data,
+      options: {},
+    };
+    const myChart = new QuickChart();
+    myChart
+      .setConfig(config)
+      .setWidth(800)
+      .setHeight(400)
+      .setBackgroundColor('transparent');
+
     const htmlFile = fs.readFileSync(
       path.join(__dirname, './something.html'),
       'utf-8'
     );
+
     sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
     const sentMessage = await sgMail.send({
       from: 'josealvarado1337@gmail.com',
       to: 'jalvarado1337@lavabit.com',
-      html: htmlFile,
+      templateId: 'd-89440f5bc55a41ee891a26c6836675c9',
       subject: 'Test Email',
+      dynamicTemplateData: {
+        quickChartUrl: myChart.getUrl(),
+      },
     });
     return res.json(sentMessage);
   } catch (error) {
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,43 @@
 import express from 'express';
+import {config} from 'dotenv';
+
+config();
 
 import emailRouter from './email/email.controller';
+import QuickChart from 'quickchart-js';
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = express();
 
 server.use(express.json());
 
 server.get('/', (req, res) => {
-  res.send('Hello world');
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },
+    ],
+  };
+  const test = {
+    type: 'line',
+    data,
+    options: {},
+  };
+  const myChart = new QuickChart();
+  myChart
+    .setConfig(test)
+    .setWidth(800)
+    .setHeight(400)
+    .setBackgroundColor('transparent');
+
+  res.send(myChart.getUrl());
 });
 
 server.use('/email', emailRouter);
